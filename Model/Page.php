4955bc49f921405725cca9e9c9a9b85587ec6f59
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2012-12-13 00:04:02 +0330

    Separate Page model from Post model.

---
+++ Model/Page.php
@@ -0,0 +1,129 @@
+<?php
+
+App::uses('AppModel', 'Model');
+
+/**
+ * Page Model
+ *
+ * @property User $User
+ * 
+ */
+class Page extends AppModel {
+
+    /**
+     * Display field
+     *
+     * @var string
+     */
+    public $displayField = 'title';
+    public $actsAs = array('Tree', 'Containable');
+
+    /**
+     * Use Table
+     * This model uses posts table.
+     *
+     * @var string
+     */
+    public $useTable = 'posts';
+
+    /**
+     * belongsTo associations
+     *
+     * @var array
+     */
+    public $belongsTo = array(
+        'User' => array(
+            'className' => 'User',
+            'foreignKey' => 'user_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        )
+    );
+
+    /**
+     * hasMany associations
+     *
+     * @var array
+     */
+    public $hasMany = array(
+        'Comment' => array(
+            'className' => 'Comment',
+            'foreignKey' => 'post_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => '',
+        ),
+    );
+
+    /**
+     * Get first page.
+     *
+     * @param intiger $page_id Page ID
+     * @return array
+     */
+    public function get_page($page_id) {
+        $post = $this->find('first', array(
+            "fields" => 'Post.id, Post.user_id',
+            "conditions" => array('Post.id' => $page_id),
+            "recursive" => 0)
+        );
+        return $post;
+    }
+
+    /**
+     * Count all pages.
+     *
+     * @return intiger Number of all pages published.
+     */
+    public function count_pages($status = 'all') {
+        switch ($status) {
+            case 'all':
+                $published = $this->find('count', array(
+                    'conditions' => array(
+                        'Page.status' => array('publish', 'draft'),
+                        'Page.type' => 'page')
+                        )
+                );
+                break;
+
+            case 'publish':
+                $published = $this->find('count', array(
+                    'conditions' => array(
+                        'Page.status' => 'publish',
+                        'Page.type' => 'page')
+                        )
+                );
+                break;
+
+            case 'draft':
+                $published = $this->find('count', array(
+                    'conditions' => array(
+                        'Page.status' => 'draft',
+                        'Page.type' => 'page')
+                        )
+                );
+                break;
+
+            case 'trash':
+                $published = $this->find('count', array(
+                    'conditions' => array(
+                        'Page.status' => 'trash',
+                        'Page.type' => 'page')
+                        )
+                );
+                break;
+
+            default:
+                break;
+        }
+        return $published;
+    }
+
+}
