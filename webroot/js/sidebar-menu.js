b2b87557524eed2692cc863f61cd3446b0306db5
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-01-01 23:18:38 +0330

    Improved sidebar-menu

--- webroot/js/sidebar-menu.js
+++ webroot/js/sidebar-menu.js
@@ -6,6 +6,19 @@ $(document).ready(function()
             $( this ).parents( ".menu:first" ).find( ".sb" ).slideToggle('normal');
 
         });
+
+        if(Hurad.params.action == "admin_index"){
+            $('a[href$="'+Hurad.params.controller+'"]').parent().addClass("current"); 
+        }
+        else if(Hurad.params.action == "admin_add"){
+            $('a[href$="'+Hurad.params.controller+'/add"]').parent().addClass("current"); 
+        }
+        else if(Hurad.params.action == "admin_prefix"){
+            $('a[href$="'+Hurad.params.pass+'"]').parent().addClass("current");
+        } 
+        
+        $('li.current').parents('li.sb').css("display", "list-item");
+        $('li.current').parents('ul.menu').find( ".top-menu" ).addClass("active");
     });
 
 
