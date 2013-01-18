7d0e02057ea70dd409fc641d694ce1fed14b293f
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-01-18 11:38:56 +0330

    Add Hurad bootstrap.

---
+++ Config/Hurad/bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+App::uses('Functions', 'Lib');
+/**
+ * Plugins
+ */
+$pluginBootstraps = Configure::read('Plugin.bootstraps');
+$plugins = array_filter(explode(',', $pluginBootstraps));
+foreach ($plugins as $plugin) {
+    $pluginName = Inflector::camelize($plugin);
+    if (!file_exists(APP . 'Plugin' . DS . $pluginName)) {
+        CakeLog::write(LOG_ERR, 'Plugin not found during bootstrap: ' . $pluginName);
+        continue;
+    }
+    $bootstrapFile = APP . 'Plugin' . DS . $pluginName . DS . 'Config' . DS . 'bootstrap.php';
+    $bootstrap = file_exists($bootstrapFile);
+    $routesFile = APP . 'Plugin' . DS . $pluginName . DS . 'Config' . DS . 'routes.php';
+    $routes = file_exists($routesFile);
+    $option = array(
+        $pluginName => array(
+            'bootstrap' => $bootstrap,
+            'routes' => $routes,
+        )
+    );
+    HrPlugin::load($option);
+}
