0cadcc7177a2e8e1686bb53d357e61feffcc95d6
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-02-27 14:16:08 +0330

    Modified LinkHelper.

--- View/Helper/LinkHelper.php
+++ View/Helper/LinkHelper.php
@@ -1,11 +1,97 @@
 <?php
-App::import('Lib', 'Functions');
+
+App::uses('AppHelper', 'View/Helper');
+
 /**
  * Description of LinkHelper
  *
  * @author mohammad
  */
 class LinkHelper extends AppHelper {
+
+    /**
+     * Other helpers used by this helper
+     *
+     * @var array
+     * @access public
+     */
+    public $helpers = array('Html', 'Time', 'Hook');
+
+    /**
+     * Current post array
+     *
+     * @var array
+     * @access public
+     */
+    public $post = array();
+
+    /**
+     * Display the permalink for the current post.
+     *
+     * @since 1.0.0
+     * @uses apply_filters() Calls 'the_permalink' filter on the permalink string.
+     */
+    function the_permalink() {
+        echo $this->Hook->apply_filters('the_permalink', $this->get_permalink());
+    }
+
+    /**
+     * Display permalink anchor for current post.
+     *
+     * The permalink mode title will use the post title for the 'a' element 'id'
+     * attribute. The id mode uses 'post-' with the post ID for the 'id' attribute.
+     *
+     * @since 1.0.0
+     *
+     * @param string $mode Permalink mode can be either 'title', 'id', or default, which is 'id'.
+     */
+    function permalink_anchor($mode = 'id') {
+        switch (strtolower($mode)) {
+            case 'title':
+                $title = Formatting::sanitize_title($this->post['Post']['title']) . '-' . $post->ID;
+                echo '<a id="' . $title . '"></a>';
+                break;
+            case 'id':
+            default:
+                echo '<a id="post-' . $this->post['Post']['id'] . '"></a>';
+                break;
+        }
+    }
+
+    /**
+     * Retrieve full permalink for current post.
+     *
+     * @since 1.0.0
+     *
+     * @return string
+     */
+    function get_permalink() {
+        $year = $this->Time->format('Y', $this->post['Post']['created']);
+        $month = $this->Time->format('m', $this->post['Post']['created']);
+        $day = $this->Time->format('d', $this->post['Post']['created']);
+        if ($this->post['Post']['type'] == 'post') {
+            switch (Configure::read('Permalink-common')) {
+                case 'default':
+                    $permalink = $this->Html->url(Configure::read('General-site_url') . "/p/" . $this->post['Post']['id']);
+                    break;
+                case 'day_name':
+                    $permalink = $this->Html->url(Configure::read('General-site_url') . "/" . $year . "/" . $month . "/" . $day . "/" . $this->post['Post']['slug']);
+                    break;
+                case 'month_name':
+                    $permalink = $this->Html->url(Configure::read('General-site_url') . "/" . $year . "/" . $month . "/" . $this->post['Post']['slug']);
+                    break;
+                default:
+                    break;
+            }
+        } elseif ($this->post['Post']['type'] == 'page') {
+            if (Configure::read('Permalink-common') == 'default') {
+                $permalink = $this->Html->url(Configure::read('General-site_url') . "/page/" . $this->post['Post']['id']);
+            } else {
+                $permalink = $this->Html->url(Configure::read('General-site_url') . "/page/" . $this->post['Post']['slug']);
+            }
+        }
+        return $this->Hook->apply_filters('post_link', $permalink);
+    }
 
     /**
      * Print the home url for the current site.
@@ -44,7 +130,7 @@ class LinkHelper extends AppHelper {
     function get_home_url($path = '', $scheme = null) {
 
         if (!in_array($scheme, array('http', 'https', 'relative'))) {
-            $scheme = is_ssl() && !is_admin() ? 'https' : 'http';
+            $scheme = Functions::is_ssl() && !is_admin() ? 'https' : 'http';
         }
 
         $url = Configure::read('General-site_url');
@@ -129,7 +215,7 @@ class LinkHelper extends AppHelper {
      */
     function get_site_url($path = '', $scheme = null) {
         if (!in_array($scheme, array('http', 'https', 'relative'))) {
-            $scheme = ( is_ssl() ? 'https' : 'http' );
+            $scheme = ( Functions::is_ssl() ? 'https' : 'http' );
         }
 
         $url = Configure::read('General-site_url');
