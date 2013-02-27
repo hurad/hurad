3626ee1f54fbb61396531e401f18af737da4304b
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-02-27 14:11:46 +0330

    Add HelloWorld plugin.

---
+++ Plugin/HelloWorld/Config/bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+$HuradHook = Configure::read('HuradHook.obj');
+HrNav::add('dashboard', array(
+    'child' => array(
+        'test' => array(
+            'title' => __('Hello World'),
+            'url' => array('admin' => TRUE, 'plugin' => 'hello_world', 'controller' => 'hello'),
+            'weight' => 1,
+        ),
+    ),
+));
+//$title .= 'Mohammad ';
+$HuradHook->add_filter('the_title', function($title) {
+            return 'Mohammad ' . $title;
+        });
+
+function test() {
+
+    echo 'Salam';
+}
+
+//$HuradHook->add_filter('the_date', 'test');
+//debug($HuradHook);
+$HuradHook->add_action('test', 'test');
+//debug($HuradHook);
---
+++ Plugin/HelloWorld/Controller/HelloWorldAppController.php
@@ -0,0 +1,8 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+class HelloWorldAppController extends AppController {
+    
+}
+
