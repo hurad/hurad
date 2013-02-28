e321165f8e9a359bdbbd0ae4b03c75eaeed87551
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-02-28 11:24:06 +0330

    Move seems_utf8() method.

--- Lib/Formatting.php
+++ Lib/Formatting.php
@@ -114,6 +114,43 @@ class Formatting {
     }
 
     /**
+     * Checks to see if a string is utf8 encoded.
+     *
+     * NOTE: This function checks for 5-Byte sequences, UTF8
+     * has Bytes Sequences with a maximum length of 4.
+     *
+     * @since 1.0.0
+     *
+     * @param string $str The string to be checked
+     * @return bool True if $str fits a UTF-8 model, false otherwise.
+     */
+    public function seems_utf8($str) {
+        $length = strlen($str);
+        for ($i = 0; $i < $length; $i++) {
+            $c = ord($str[$i]);
+            if ($c < 0x80)
+                $n = 0;# 0bbbbbbb
+            elseif (($c & 0xE0) == 0xC0)
+                $n = 1;# 110bbbbb
+            elseif (($c & 0xF0) == 0xE0)
+                $n = 2;# 1110bbbb
+            elseif (($c & 0xF8) == 0xF0)
+                $n = 3;# 11110bbb
+            elseif (($c & 0xFC) == 0xF8)
+                $n = 4;# 111110bb
+            elseif (($c & 0xFE) == 0xFC)
+                $n = 5;# 1111110b
+            else
+                return false;# Does not match any model
+            for ($j = 0; $j < $n; $j++) { # n bytes matching 10bbbbbb follow ?
+                if ((++$i == $length) || ((ord($str[$i]) & 0xC0) != 0x80))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    /**
      * Converts a number of characters from a string.
      *
      * Metadata tags <<title>> and <<category>> are removed, <<br>> and <<hr>> are
@@ -291,6 +328,8 @@ class Formatting {
         // Empty Stack
         while ($x = array_pop($tagstack))
             $newtext .= '</' . $x . '>'; // Add remaining tags to close
+
+
 
 
 
@@ -565,44 +604,6 @@ class Formatting {
         }
 
         return $string;
-    }
-
-    /**
-     * Checks to see if a string is utf8 encoded.
-     *
-     * NOTE: This function checks for 5-Byte sequences, UTF8
-     * has Bytes Sequences with a maximum length of 4.
-     *
-     * @author bmorel at ssi dot fr (modified)
-     * @since 1.0.0
-     *
-     * @param string $str The string to be checked
-     * @return bool True if $str fits a UTF-8 model, false otherwise.
-     */
-    function seems_utf8($str) {
-        $length = strlen($str);
-        for ($i = 0; $i < $length; $i++) {
-            $c = ord($str[$i]);
-            if ($c < 0x80)
-                $n = 0;# 0bbbbbbb
-            elseif (($c & 0xE0) == 0xC0)
-                $n = 1;# 110bbbbb
-            elseif (($c & 0xF0) == 0xE0)
-                $n = 2;# 1110bbbb
-            elseif (($c & 0xF8) == 0xF0)
-                $n = 3;# 11110bbb
-            elseif (($c & 0xFC) == 0xF8)
-                $n = 4;# 111110bb
-            elseif (($c & 0xFE) == 0xFC)
-                $n = 5;# 1111110b
-            else
-                return false;# Does not match any model
-            for ($j = 0; $j < $n; $j++) { # n bytes matching 10bbbbbb follow ?
-                if ((++$i == $length) || ((ord($str[$i]) & 0xC0) != 0x80))
-                    return false;
-            }
-        }
-        return true;
     }
 
     /**
