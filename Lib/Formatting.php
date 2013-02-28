61673cb15cdd9460cad93531e9f78b61108c019c
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-02-28 12:11:52 +0330

    Move remove_accents() method.

--- Lib/Formatting.php
+++ Lib/Formatting.php
@@ -458,173 +458,6 @@ class Formatting {
     }
 
     /**
-     * Balances tags of string using a modified stack.
-     *
-     * @since 1.0.0
-     *
-     * @param string $text Text to be balanced.
-     * @return string Balanced text.
-     */
-    function force_balance_tags($text) {
-        $tagstack = array();
-        $stacksize = 0;
-        $tagqueue = '';
-        $newtext = '';
-        // Known single-entity/self-closing tags
-        $single_tags = array('area', 'base', 'basefont', 'br', 'col', 'command', 'embed', 'frame', 'hr', 'img', 'input', 'isindex', 'link', 'meta', 'param', 'source');
-        // Tags that can be immediately nested within themselves
-        $nestable_tags = array('blockquote', 'div', 'object', 'q', 'span');
-
-        // HR bug fix for comments - in case you REALLY meant to type '< !--'
-        $text = str_replace('< !--', '< !--', $text);
-        // HR bug fix for LOVE <3 (and other situations with '<' before a number)
-        $text = preg_replace('#<([0-9]{1})#', '&lt;$1', $text);
-
-        while (preg_match("/<(\/?[\w:]*)\s*([^>]*)>/", $text, $regex)) {
-            $newtext .= $tagqueue;
-
-            $i = strpos($text, $regex[0]);
-            $l = strlen($regex[0]);
-
-            // clear the shifter
-            $tagqueue = '';
-            // Pop or Push
-            if (isset($regex[1][0]) && '/' == $regex[1][0]) { // End Tag
-                $tag = strtolower(substr($regex[1], 1));
-                // if too many closing tags
-                if ($stacksize <= 0) {
-                    $tag = '';
-                    // or close to be safe $tag = '/' . $tag;
-                }
-                // if stacktop value = tag close value then pop
-                else if ($tagstack[$stacksize - 1] == $tag) { // found closing tag
-                    $tag = '</' . $tag . '>'; // Close Tag
-                    // Pop
-                    array_pop($tagstack);
-                    $stacksize--;
-                } else { // closing tag not at top, search for it
-                    for ($j = $stacksize - 1; $j >= 0; $j--) {
-                        if ($tagstack[$j] == $tag) {
-                            // add tag to tagqueue
-                            for ($k = $stacksize - 1; $k >= $j; $k--) {
-                                $tagqueue .= '</' . array_pop($tagstack) . '>';
-                                $stacksize--;
-                            }
-                            break;
-                        }
-                    }
-                    $tag = '';
-                }
-            } else { // Begin Tag
-                $tag = strtolower($regex[1]);
-
-                // Tag Cleaning
-                // If it's an empty tag "< >", do nothing
-                if ('' == $tag) {
-                    // do nothing
-                }
-                // ElseIf it presents itself as a self-closing tag...
-                elseif (substr($regex[2], -1) == '/') {
-                    // ...but it isn't a known single-entity self-closing tag, then don't let it be treated as such and
-                    // immediately close it with a closing tag (the tag will encapsulate no text as a result)
-                    if (!in_array($tag, $single_tags))
-                        $regex[2] = trim(substr($regex[2], 0, -1)) . "></$tag";
-                }
-                // ElseIf it's a known single-entity tag but it doesn't close itself, do so
-                elseif (in_array($tag, $single_tags)) {
-                    $regex[2] .= '/';
-                }
-                // Else it's not a single-entity tag
-                else {
-                    // If the top of the stack is the same as the tag we want to push, close previous tag
-                    if ($stacksize > 0 && !in_array($tag, $nestable_tags) && $tagstack[$stacksize - 1] == $tag) {
-                        $tagqueue = '</' . array_pop($tagstack) . '>';
-                        $stacksize--;
-                    }
-                    $stacksize = array_push($tagstack, $tag);
-                }
-
-                // Attributes
-                $attributes = $regex[2];
-                if (!empty($attributes) && $attributes[0] != '>')
-                    $attributes = ' ' . $attributes;
-
-                $tag = '<' . $tag . $attributes . '>';
-                //If already queuing a close tag, then put this tag on, too
-                if (!empty($tagqueue)) {
-                    $tagqueue .= $tag;
-                    $tag = '';
-                }
-            }
-            $newtext .= substr($text, 0, $i) . $tag;
-            $text = substr($text, $i + $l);
-        }
-
-        // Clear Tag Queue
-        $newtext .= $tagqueue;
-
-        // Add Remaining text
-        $newtext .= $text;
-
-        // Empty Stack
-        while ($x = array_pop($tagstack))
-            $newtext .= '</' . $x . '>'; // Add remaining tags to close
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-// HR fix for the bug with HTML comments
-        $newtext = str_replace("< !--", "<!--", $newtext);
-        $newtext = str_replace("< !--", "< !--", $newtext);
-
-        return $newtext;
-    }
-
-    /**
-     * Sanitizes a title, or returns a fallback title.
-     *
-     * Specifically, HTML and PHP tags are stripped. Further actions can be added
-     * via the plugin API. If $title is empty and $fallback_title is set, the latter
-     * will be used.
-     *
-     * @since 1.0.0
-     *
-     * @param string $title The string to be sanitized.
-     * @param string $fallback_title Optional. A title to use if $title is empty.
-     * @param string $context Optional. The operation for which the string is sanitized
-     * @return string The sanitized string.
-     */
-    function sanitize_title($title, $fallback_title = '', $context = 'save') {
-        $raw_title = $title;
-
-        if ('save' == $context)
-            $title = $this->remove_accents($title);
-
-        $title = $this->HuradHook->apply_filters('sanitize_title', $title, $raw_title, $context);
-
-        if ('' === $title || false === $title)
-            $title = $fallback_title;
-
-        return $title;
-    }
-
-    /**
      * Converts all accent characters to ASCII characters.
      *
      * If there are no accent characters, then the string given is just returned.
@@ -634,7 +467,7 @@ class Formatting {
      * @param string $string Text that might have accent characters
      * @return string Filtered string with replaced "nice" characters.
      */
-    function remove_accents($string) {
+    public function remove_accents($string) {
         if (!preg_match('/[\x80-\xff]/', $string))
             return $string;
 
@@ -852,6 +685,174 @@ class Formatting {
         }
 
         return $string;
+    }
+
+    /**
+     * Balances tags of string using a modified stack.
+     *
+     * @since 1.0.0
+     *
+     * @param string $text Text to be balanced.
+     * @return string Balanced text.
+     */
+    function force_balance_tags($text) {
+        $tagstack = array();
+        $stacksize = 0;
+        $tagqueue = '';
+        $newtext = '';
+        // Known single-entity/self-closing tags
+        $single_tags = array('area', 'base', 'basefont', 'br', 'col', 'command', 'embed', 'frame', 'hr', 'img', 'input', 'isindex', 'link', 'meta', 'param', 'source');
+        // Tags that can be immediately nested within themselves
+        $nestable_tags = array('blockquote', 'div', 'object', 'q', 'span');
+
+        // HR bug fix for comments - in case you REALLY meant to type '< !--'
+        $text = str_replace('< !--', '< !--', $text);
+        // HR bug fix for LOVE <3 (and other situations with '<' before a number)
+        $text = preg_replace('#<([0-9]{1})#', '&lt;$1', $text);
+
+        while (preg_match("/<(\/?[\w:]*)\s*([^>]*)>/", $text, $regex)) {
+            $newtext .= $tagqueue;
+
+            $i = strpos($text, $regex[0]);
+            $l = strlen($regex[0]);
+
+            // clear the shifter
+            $tagqueue = '';
+            // Pop or Push
+            if (isset($regex[1][0]) && '/' == $regex[1][0]) { // End Tag
+                $tag = strtolower(substr($regex[1], 1));
+                // if too many closing tags
+                if ($stacksize <= 0) {
+                    $tag = '';
+                    // or close to be safe $tag = '/' . $tag;
+                }
+                // if stacktop value = tag close value then pop
+                else if ($tagstack[$stacksize - 1] == $tag) { // found closing tag
+                    $tag = '</' . $tag . '>'; // Close Tag
+                    // Pop
+                    array_pop($tagstack);
+                    $stacksize--;
+                } else { // closing tag not at top, search for it
+                    for ($j = $stacksize - 1; $j >= 0; $j--) {
+                        if ($tagstack[$j] == $tag) {
+                            // add tag to tagqueue
+                            for ($k = $stacksize - 1; $k >= $j; $k--) {
+                                $tagqueue .= '</' . array_pop($tagstack) . '>';
+                                $stacksize--;
+                            }
+                            break;
+                        }
+                    }
+                    $tag = '';
+                }
+            } else { // Begin Tag
+                $tag = strtolower($regex[1]);
+
+                // Tag Cleaning
+                // If it's an empty tag "< >", do nothing
+                if ('' == $tag) {
+                    // do nothing
+                }
+                // ElseIf it presents itself as a self-closing tag...
+                elseif (substr($regex[2], -1) == '/') {
+                    // ...but it isn't a known single-entity self-closing tag, then don't let it be treated as such and
+                    // immediately close it with a closing tag (the tag will encapsulate no text as a result)
+                    if (!in_array($tag, $single_tags))
+                        $regex[2] = trim(substr($regex[2], 0, -1)) . "></$tag";
+                }
+                // ElseIf it's a known single-entity tag but it doesn't close itself, do so
+                elseif (in_array($tag, $single_tags)) {
+                    $regex[2] .= '/';
+                }
+                // Else it's not a single-entity tag
+                else {
+                    // If the top of the stack is the same as the tag we want to push, close previous tag
+                    if ($stacksize > 0 && !in_array($tag, $nestable_tags) && $tagstack[$stacksize - 1] == $tag) {
+                        $tagqueue = '</' . array_pop($tagstack) . '>';
+                        $stacksize--;
+                    }
+                    $stacksize = array_push($tagstack, $tag);
+                }
+
+                // Attributes
+                $attributes = $regex[2];
+                if (!empty($attributes) && $attributes[0] != '>')
+                    $attributes = ' ' . $attributes;
+
+                $tag = '<' . $tag . $attributes . '>';
+                //If already queuing a close tag, then put this tag on, too
+                if (!empty($tagqueue)) {
+                    $tagqueue .= $tag;
+                    $tag = '';
+                }
+            }
+            $newtext .= substr($text, 0, $i) . $tag;
+            $text = substr($text, $i + $l);
+        }
+
+        // Clear Tag Queue
+        $newtext .= $tagqueue;
+
+        // Add Remaining text
+        $newtext .= $text;
+
+        // Empty Stack
+        while ($x = array_pop($tagstack))
+            $newtext .= '</' . $x . '>'; // Add remaining tags to close
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+// HR fix for the bug with HTML comments
+        $newtext = str_replace("< !--", "<!--", $newtext);
+        $newtext = str_replace("< !--", "< !--", $newtext);
+
+        return $newtext;
+    }
+
+    /**
+     * Sanitizes a title, or returns a fallback title.
+     *
+     * Specifically, HTML and PHP tags are stripped. Further actions can be added
+     * via the plugin API. If $title is empty and $fallback_title is set, the latter
+     * will be used.
+     *
+     * @since 1.0.0
+     *
+     * @param string $title The string to be sanitized.
+     * @param string $fallback_title Optional. A title to use if $title is empty.
+     * @param string $context Optional. The operation for which the string is sanitized
+     * @return string The sanitized string.
+     */
+    function sanitize_title($title, $fallback_title = '', $context = 'save') {
+        $raw_title = $title;
+
+        if ('save' == $context)
+            $title = $this->remove_accents($title);
+
+        $title = $this->HuradHook->apply_filters('sanitize_title', $title, $raw_title, $context);
+
+        if ('' === $title || false === $title)
+            $title = $fallback_title;
+
+        return $title;
     }
 
     /**
