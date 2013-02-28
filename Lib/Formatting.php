8f81637dce7a21224d1035ee4c4609651c39a353
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-02-28 15:03:36 +0330

    Add _split_str_by_whitespace() method for breaks a string into chunks by splitting at whitespace characters.

--- Lib/Formatting.php
+++ Lib/Formatting.php
@@ -1351,6 +1351,64 @@ class Formatting {
     }
 
     /**
+     * Breaks a string into chunks by splitting at whitespace characters.
+     * The length of each returned chunk is as close to the specified length goal as possible,
+     * with the caveat that each chunk includes its trailing delimiter.
+     * Chunks longer than the goal are guaranteed to not have any inner whitespace.
+     *
+     * Joining the returned chunks with empty delimiters reconstructs the input string losslessly.
+     *
+     * Input string must have no null characters (or eventual transformations on output chunks must not care about null characters)
+     *
+     * <code>
+     * _split_str_by_whitespace( "1234 67890 1234 67890a cd 1234 890 123456789 1234567890a 45678 1 3 5 7 90 ", 10 ) ==
+     * array (
+     * 0 => '1234 67890 ', // 11 characters: Perfect split
+     * 1 => '1234 ', // 5 characters: '1234 67890a' was too long
+     * 2 => '67890a cd ', // 10 characters: '67890a cd 1234' was too long
+     * 3 => '1234 890 ', // 11 characters: Perfect split
+     * 4 => '123456789 ', // 10 characters: '123456789 1234567890a' was too long
+     * 5 => '1234567890a ', // 12 characters: Too long, but no inner whitespace on which to split
+     * 6 => ' 45678 ', // 11 characters: Perfect split
+     * 7 => '1 3 5 7 9', // 9 characters: End of $string
+     * );
+     * </code>
+     *
+     * @since 1.0.0
+     * @access private
+     *
+     * @param string $string The string to split.
+     * @param int $goal The desired chunk length.
+     * @return array Numeric array of chunks.
+     */
+    private function _split_str_by_whitespace($string, $goal) {
+        $chunks = array();
+
+        $string_nullspace = strtr($string, "\r\n\t\v\f ", "\000\000\000\000\000\000");
+
+        while ($goal < strlen($string_nullspace)) {
+            $pos = strrpos(substr($string_nullspace, 0, $goal + 1), "\000");
+
+            if (false === $pos) {
+                $pos = strpos($string_nullspace, "\000", $goal + 1);
+                if (false === $pos) {
+                    break;
+                }
+            }
+
+            $chunks[] = substr($string, 0, $pos + 1);
+            $string = substr($string, $pos + 1);
+            $string_nullspace = substr($string_nullspace, $pos + 1);
+        }
+
+        if ($string) {
+            $chunks[] = $string;
+        }
+
+        return $chunks;
+    }
+
+    /**
      * Sanitize a string from user input or from the db
      *
      * check for invalid UTF-8,
