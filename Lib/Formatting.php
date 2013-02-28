bed72f9d2e4400be40d4a88df8d1991cfb38ab32
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-02-28 11:44:45 +0330

    Change hr_check_invalid_utf8 methods.

--- Lib/Formatting.php
+++ Lib/Formatting.php
@@ -359,6 +359,54 @@ class Formatting {
     }
 
     /**
+     * Checks for invalid UTF8 in a string.
+     *
+     * @since 1.0.0
+     *
+     * @param string $string The text which is to be checked.
+     * @param boolean $strip Optional. Whether to attempt to strip out invalid UTF8. Default is false.
+     * @return string The checked text.
+     */
+    public function hr_check_invalid_utf8($string, $strip = false) {
+        $string = (string) $string;
+
+        if (0 === strlen($string)) {
+            return '';
+        }
+
+        // Store the site charset as a static to avoid multiple calls to get_option()
+        static $is_utf8;
+        if (!isset($is_utf8)) {
+            $is_utf8 = in_array(Configure::read('site_charset'), array('utf8', 'utf-8', 'UTF8', 'UTF-8'));
+        }
+        if (!$is_utf8) {
+            return $string;
+        }
+
+        // Check for support for utf8 in the installed PCRE library once and store the result in a static
+        static $utf8_pcre;
+        if (!isset($utf8_pcre)) {
+            $utf8_pcre = @preg_match('/^./u', 'a');
+        }
+        // We can't demand utf8 in the PCRE installation, so just return the string in those cases
+        if (!$utf8_pcre) {
+            return $string;
+        }
+
+        // preg_match fails when it encounters invalid UTF8 in $string
+        if (1 === @preg_match('/^./us', $string)) {
+            return $string;
+        }
+
+        // Attempt to strip the bad chars if requested (not recommended)
+        if ($strip && function_exists('iconv')) {
+            return iconv('utf-8', 'utf-8', $string);
+        }
+
+        return '';
+    }
+
+    /**
      * Balances tags of string using a modified stack.
      *
      * @since 1.0.0
@@ -470,6 +518,8 @@ class Formatting {
         // Empty Stack
         while ($x = array_pop($tagstack))
             $newtext .= '</' . $x . '>'; // Add remaining tags to close
+
+
 
 
 
@@ -828,54 +878,6 @@ class Formatting {
 
         //return apply_filters('clean_url', $url, $original_url, $_context);
         return $url;
-    }
-
-    /**
-     * Checks for invalid UTF8 in a string.
-     *
-     * @since 1.0
-     *
-     * @param string $string The text which is to be checked.
-     * @param boolean $strip Optional. Whether to attempt to strip out invalid UTF8. Default is false.
-     * @return string The checked text.
-     */
-    function hr_check_invalid_utf8($string, $strip = false) {
-        $string = (string) $string;
-
-        if (0 === strlen($string)) {
-            return '';
-        }
-
-        // Store the site charset as a static to avoid multiple calls to get_option()
-        static $is_utf8;
-        if (!isset($is_utf8)) {
-            $is_utf8 = in_array(Configure::read('site_charset'), array('utf8', 'utf-8', 'UTF8', 'UTF-8'));
-        }
-        if (!$is_utf8) {
-            return $string;
-        }
-
-        // Check for support for utf8 in the installed PCRE library once and store the result in a static
-        static $utf8_pcre;
-        if (!isset($utf8_pcre)) {
-            $utf8_pcre = @preg_match('/^./u', 'a');
-        }
-        // We can't demand utf8 in the PCRE installation, so just return the string in those cases
-        if (!$utf8_pcre) {
-            return $string;
-        }
-
-        // preg_match fails when it encounters invalid UTF8 in $string
-        if (1 === @preg_match('/^./us', $string)) {
-            return $string;
-        }
-
-        // Attempt to strip the bad chars if requested (not recommended)
-        if ($strip && function_exists('iconv')) {
-            return iconv('utf-8', 'utf-8', $string);
-        }
-
-        return '';
     }
 
     /**
