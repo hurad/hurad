334e811a8731c99edb5b34f3b6f3f6e4688d9e14
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-02-28 11:36:40 +0330

    Change _hr_specialchars() and hr_specialchars_decode() methods.

--- Lib/Formatting.php
+++ Lib/Formatting.php
@@ -151,6 +151,148 @@ class Formatting {
     }
 
     /**
+     * Converts a number of special characters into their HTML entities.
+     *
+     * Specifically deals with: &, <, >, ", and '.
+     *
+     * $quote_style can be set to ENT_COMPAT to encode " to
+     * &quot;, or ENT_QUOTES to do both. Default is ENT_NOQUOTES where no quotes are encoded.
+     *
+     * @since 1.0.0
+     * @access private
+     *
+     * @param string $string The text which is to be encoded.
+     * @param mixed $quote_style Optional. Converts double quotes if set to ENT_COMPAT, both single and double if set to ENT_QUOTES or none if set to ENT_NOQUOTES. Also compatible with old values; converting single quotes if set to 'single', double if set to 'double' or both if otherwise set. Default is ENT_NOQUOTES.
+     * @param string $charset Optional. The character encoding of the string. Default is false.
+     * @param boolean $double_encode Optional. Whether to encode existing html entities. Default is false.
+     * @return string The encoded text with HTML entities.
+     */
+    private function _hr_specialchars($string, $quote_style = ENT_NOQUOTES, $charset = false, $double_encode = false) {
+        $string = (string) $string;
+
+        if (0 === strlen($string))
+            return '';
+
+        // Don't bother if there are no specialchars - saves some processing
+        if (!preg_match('/[&<>"\']/', $string))
+            return $string;
+
+        // Account for the previous behaviour of the function when the $quote_style is not an accepted value
+        if (empty($quote_style))
+            $quote_style = ENT_NOQUOTES;
+        elseif (!in_array($quote_style, array(0, 2, 3, 'single', 'double'), true))
+            $quote_style = ENT_QUOTES;
+
+        // Store the site charset as a static to avoid multiple calls to wp_load_alloptions()
+        if (!$charset) {
+            static $_charset;
+            if (!isset($_charset)) {
+                $_charset = isset(Configure::read('site_charset')) ? Configure::read('site_charset') : '';
+            }
+            $charset = $_charset;
+        }
+
+        if (in_array($charset, array('utf8', 'utf-8', 'UTF8')))
+            $charset = 'UTF-8';
+
+        $_quote_style = $quote_style;
+
+        if ($quote_style === 'double') {
+            $quote_style = ENT_COMPAT;
+            $_quote_style = ENT_COMPAT;
+        } elseif ($quote_style === 'single') {
+            $quote_style = ENT_NOQUOTES;
+        }
+
+        // Handle double encoding ourselves
+        if ($double_encode) {
+            $string = @htmlspecialchars($string, $quote_style, $charset);
+        } else {
+            // Decode &amp; into &
+            $string = $this->hr_specialchars_decode($string, $_quote_style);
+
+            // Guarantee every &entity; is valid or re-encode the &
+            $string = $kses->normalizeEntities($string);
+
+            // Now re-encode everything except &entity;
+            $string = preg_split('/(&#?x?[0-9a-z]+;)/i', $string, -1, PREG_SPLIT_DELIM_CAPTURE);
+
+            for ($i = 0; $i < count($string); $i += 2)
+                $string[$i] = @htmlspecialchars($string[$i], $quote_style, $charset);
+
+            $string = implode('', $string);
+        }
+
+        // Backwards compatibility
+        if ('single' === $_quote_style)
+            $string = str_replace("'", '&#039;', $string);
+
+        return $string;
+    }
+
+    /**
+     * Converts a number of HTML entities into their special characters.
+     *
+     * Specifically deals with: &, <, >, ", and '.
+     *
+     * $quote_style can be set to ENT_COMPAT to decode " entities,
+     * or ENT_QUOTES to do both " and '. Default is ENT_NOQUOTES where no quotes are decoded.
+     *
+     * @since 1.0.0
+     *
+     * @param string $string The text which is to be decoded.
+     * @param mixed $quote_style Optional. Converts double quotes if set to ENT_COMPAT, both single and double if set to ENT_QUOTES or none if set to ENT_NOQUOTES. Also compatible with old _wp_specialchars() values; converting single quotes if set to 'single', double if set to 'double' or both if otherwise set. Default is ENT_NOQUOTES.
+     * @return string The decoded text without HTML entities.
+     */
+    public function hr_specialchars_decode($string, $quote_style = ENT_NOQUOTES) {
+        $string = (string) $string;
+
+        if (0 === strlen($string)) {
+            return '';
+        }
+
+        // Don't bother if there are no entities - saves a lot of processing
+        if (strpos($string, '&') === false) {
+            return $string;
+        }
+
+        // Match the previous behaviour of _wp_specialchars() when the $quote_style is not an accepted value
+        if (empty($quote_style)) {
+            $quote_style = ENT_NOQUOTES;
+        } elseif (!in_array($quote_style, array(0, 2, 3, 'single', 'double'), true)) {
+            $quote_style = ENT_QUOTES;
+        }
+
+        // More complete than get_html_translation_table( HTML_SPECIALCHARS )
+        $single = array('&#039;' => '\'', '&#x27;' => '\'');
+        $single_preg = array('/&#0*39;/' => '&#039;', '/&#x0*27;/i' => '&#x27;');
+        $double = array('&quot;' => '"', '&#034;' => '"', '&#x22;' => '"');
+        $double_preg = array('/&#0*34;/' => '&#034;', '/&#x0*22;/i' => '&#x22;');
+        $others = array('&lt;' => '<', '&#060;' => '<', '&gt;' => '>', '&#062;' => '>', '&amp;' => '&', '&#038;' => '&', '&#x26;' => '&');
+        $others_preg = array('/&#0*60;/' => '&#060;', '/&#0*62;/' => '&#062;', '/&#0*38;/' => '&#038;', '/&#x0*26;/i' => '&#x26;');
+
+        if ($quote_style === ENT_QUOTES) {
+            $translation = array_merge($single, $double, $others);
+            $translation_preg = array_merge($single_preg, $double_preg, $others_preg);
+        } elseif ($quote_style === ENT_COMPAT || $quote_style === 'double') {
+            $translation = array_merge($double, $others);
+            $translation_preg = array_merge($double_preg, $others_preg);
+        } elseif ($quote_style === 'single') {
+            $translation = array_merge($single, $others);
+            $translation_preg = array_merge($single_preg, $others_preg);
+        } elseif ($quote_style === ENT_NOQUOTES) {
+            $translation = $others;
+            $translation_preg = $others_preg;
+        }
+
+        // Remove zero padding on numeric entities
+        $string = preg_replace(array_keys($translation_preg), array_values($translation_preg), $string);
+
+        // Replace characters according to translation table
+        return strtr($string, $translation);
+    }
+
+    /**
      * Converts a number of characters from a string.
      *
      * Metadata tags <<title>> and <<category>> are removed, <<br>> and <<hr>> are
@@ -328,6 +470,10 @@ class Formatting {
         // Empty Stack
         while ($x = array_pop($tagstack))
             $newtext .= '</' . $x . '>'; // Add remaining tags to close
+
+
+
+
 
 
 
@@ -730,148 +876,6 @@ class Formatting {
         }
 
         return '';
-    }
-
-    /**
-     * Converts a number of special characters into their HTML entities.
-     *
-     * Specifically deals with: &, <, >, ", and '.
-     *
-     * $quote_style can be set to ENT_COMPAT to encode " to
-     * &quot;, or ENT_QUOTES to do both. Default is ENT_NOQUOTES where no quotes are encoded.
-     *
-     * @since 1.0
-     *
-     * @param string $string The text which is to be encoded.
-     * @param mixed $quote_style Optional. Converts double quotes if set to ENT_COMPAT, both single and double if set to ENT_QUOTES or none if set to ENT_NOQUOTES. Also compatible with old values; converting single quotes if set to 'single', double if set to 'double' or both if otherwise set. Default is ENT_NOQUOTES.
-     * @param string $charset Optional. The character encoding of the string. Default is false.
-     * @param boolean $double_encode Optional. Whether to encode existing html entities. Default is false.
-     * @return string The encoded text with HTML entities.
-     */
-    function _hr_specialchars($string, $quote_style = ENT_NOQUOTES, $charset = false, $double_encode = false) {
-        $string = (string) $string;
-
-        if (0 === strlen($string))
-            return '';
-
-        // Don't bother if there are no specialchars - saves some processing
-        if (!preg_match('/[&<>"\']/', $string))
-            return $string;
-
-        // Account for the previous behaviour of the function when the $quote_style is not an accepted value
-        if (empty($quote_style))
-            $quote_style = ENT_NOQUOTES;
-        elseif (!in_array($quote_style, array(0, 2, 3, 'single', 'double'), true))
-            $quote_style = ENT_QUOTES;
-
-        // Store the site charset as a static to avoid multiple calls to wp_load_alloptions()
-        if (!$charset) {
-            static $_charset;
-            if (!isset($_charset)) {
-                $alloptions = wp_load_alloptions();
-                $_charset = isset($alloptions['blog_charset']) ? $alloptions['blog_charset'] : '';
-            }
-            $charset = $_charset;
-        }
-
-        if (in_array($charset, array('utf8', 'utf-8', 'UTF8')))
-            $charset = 'UTF-8';
-
-        $_quote_style = $quote_style;
-
-        if ($quote_style === 'double') {
-            $quote_style = ENT_COMPAT;
-            $_quote_style = ENT_COMPAT;
-        } elseif ($quote_style === 'single') {
-            $quote_style = ENT_NOQUOTES;
-        }
-
-        // Handle double encoding ourselves
-        if ($double_encode) {
-            $string = @htmlspecialchars($string, $quote_style, $charset);
-        } else {
-            // Decode &amp; into &
-            $string = $this->hr_specialchars_decode($string, $_quote_style);
-
-            // Guarantee every &entity; is valid or re-encode the &
-            $string = $kses->normalizeEntities($string);
-
-            // Now re-encode everything except &entity;
-            $string = preg_split('/(&#?x?[0-9a-z]+;)/i', $string, -1, PREG_SPLIT_DELIM_CAPTURE);
-
-            for ($i = 0; $i < count($string); $i += 2)
-                $string[$i] = @htmlspecialchars($string[$i], $quote_style, $charset);
-
-            $string = implode('', $string);
-        }
-
-        // Backwards compatibility
-        if ('single' === $_quote_style)
-            $string = str_replace("'", '&#039;', $string);
-
-        return $string;
-    }
-
-    /**
-     * Converts a number of HTML entities into their special characters.
-     *
-     * Specifically deals with: &, <, >, ", and '.
-     *
-     * $quote_style can be set to ENT_COMPAT to decode " entities,
-     * or ENT_QUOTES to do both " and '. Default is ENT_NOQUOTES where no quotes are decoded.
-     *
-     * @since 1.0
-     *
-     * @param string $string The text which is to be decoded.
-     * @param mixed $quote_style Optional. Converts double quotes if set to ENT_COMPAT, both single and double if set to ENT_QUOTES or none if set to ENT_NOQUOTES. Also compatible with old _wp_specialchars() values; converting single quotes if set to 'single', double if set to 'double' or both if otherwise set. Default is ENT_NOQUOTES.
-     * @return string The decoded text without HTML entities.
-     */
-    function hr_specialchars_decode($string, $quote_style = ENT_NOQUOTES) {
-        $string = (string) $string;
-
-        if (0 === strlen($string)) {
-            return '';
-        }
-
-        // Don't bother if there are no entities - saves a lot of processing
-        if (strpos($string, '&') === false) {
-            return $string;
-        }
-
-        // Match the previous behaviour of _hr_specialchars() when the $quote_style is not an accepted value
-        if (empty($quote_style)) {
-            $quote_style = ENT_NOQUOTES;
-        } elseif (!in_array($quote_style, array(0, 2, 3, 'single', 'double'), true)) {
-            $quote_style = ENT_QUOTES;
-        }
-
-        // More complete than get_html_translation_table( HTML_SPECIALCHARS )
-        $single = array('&#039;' => '\'', '&#x27;' => '\'');
-        $single_preg = array('/&#0*39;/' => '&#039;', '/&#x0*27;/i' => '&#x27;');
-        $double = array('&quot;' => '"', '&#034;' => '"', '&#x22;' => '"');
-        $double_preg = array('/&#0*34;/' => '&#034;', '/&#x0*22;/i' => '&#x22;');
-        $others = array('&lt;' => '<', '&#060;' => '<', '&gt;' => '>', '&#062;' => '>', '&amp;' => '&', '&#038;' => '&', '&#x26;' => '&');
-        $others_preg = array('/&#0*60;/' => '&#060;', '/&#0*62;/' => '&#062;', '/&#0*38;/' => '&#038;', '/&#x0*26;/i' => '&#x26;');
-
-        if ($quote_style === ENT_QUOTES) {
-            $translation = array_merge($single, $double, $others);
-            $translation_preg = array_merge($single_preg, $double_preg, $others_preg);
-        } elseif ($quote_style === ENT_COMPAT || $quote_style === 'double') {
-            $translation = array_merge($double, $others);
-            $translation_preg = array_merge($double_preg, $others_preg);
-        } elseif ($quote_style === 'single') {
-            $translation = array_merge($single, $others);
-            $translation_preg = array_merge($single_preg, $others_preg);
-        } elseif ($quote_style === ENT_NOQUOTES) {
-            $translation = $others;
-            $translation_preg = $others_preg;
-        }
-
-        // Remove zero padding on numeric entities
-        $string = preg_replace(array_keys($translation_preg), array_values($translation_preg), $string);
-
-        // Replace characters according to translation table
-        return strtr($string, $translation);
     }
 
     /**
