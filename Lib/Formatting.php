a61c1dc488f4c4fa14464bd5cd763957b708d6b3
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-02-28 14:28:30 +0330

    Add zeroise() method for add leading zeros when necessary.

--- Lib/Formatting.php
+++ Lib/Formatting.php
@@ -1088,6 +1088,27 @@ class Formatting {
     }
 
     /**
+     * Add leading zeros when necessary.
+     *
+     * If you set the threshold to '4' and the number is '10', then you will get
+     * back '0010'. If you set the threshold to '4' and the number is '5000', then you
+     * will get back '5000'.
+     *
+     * Uses sprintf to append the amount of zeros based on the $threshold parameter
+     * and the size of the number. If the number is large enough, then no zeros will
+     * be appended.
+     *
+     * @since 1.0.0
+     *
+     * @param mixed $number Number to append zeros to if not greater than threshold.
+     * @param int $threshold Digit places number needs to be to not have zeros added.
+     * @return string Adds leading zeros to number if needed.
+     */
+    public function zeroise($number, $threshold) {
+        return sprintf('%0' . $threshold . 's', $number);
+    }
+
+    /**
      * Sanitize a string from user input or from the db
      *
      * check for invalid UTF-8,
