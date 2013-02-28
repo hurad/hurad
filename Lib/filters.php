f94d24c276a4cfd158ec02d8f5ed5b152387a070
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-02-28 23:01:46 +0330

    Added new filters.

--- Lib/filters.php
+++ Lib/filters.php
@@ -15,4 +15,10 @@ $HuradHook->add_filter('the_content', 'Formatting::convert_chars');
 //foreach (array('the_content', 'the_title') as $filter) {
 //    $HuradHook->add_filter($filter, 'capital_P_dangit', 11);
 //}
-    
+
+//$HuradHook->add_filter( 'comment_text', 'wptexturize' );
+$HuradHook->add_filter( 'comment_text', 'Formatting::convert_chars' );
+$HuradHook->add_filter( 'comment_text', 'Formatting::make_clickable', 9 );
+$HuradHook->add_filter( 'comment_text', 'Formatting::force_balance_tags', 25 );
+//$HuradHook->add_filter( 'comment_text', 'convert_smilies', 20 );
+$HuradHook->add_filter( 'comment_text', 'Formatting::hrautop', 30 );
