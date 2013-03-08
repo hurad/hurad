dfac81303bebe07768c5609aaab152a649705e90
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-03-08 20:30:09 +0330

    Enable isAuthorized() in beforeFilter() method and changing in isAuthorized() method.

--- Controller/CategoriesController.php
+++ Controller/CategoriesController.php
@@ -18,122 +18,25 @@ class CategoriesController extends AppController {
 
     public function beforeFilter() {
         parent::beforeFilter();
-        $this->Auth->allow('*');
-        //$this->isAuthorized();
+        $this->isAuthorized();
     }
 
     public function isAuthorized() {
         switch ($this->Auth->user('role')) {
             case 'admin':
-                $this->Auth->allow('*');
+                $this->Auth->allow();
                 break;
+            case 'editor':
+                $this->Auth->allow('admin_index', 'admin_edit', 'admin_add');
+                break;
+            case 'author':
             case 'user':
-                $this->Auth->allow('index', 'profile', 'change_password', 'login', 'logout');
+                $this->Auth->allow('admin_index');
+                break;
             default :
-                $this->Auth->allow('login', 'logout', 'view', 'register');
+                return FALSE;
                 break;
         }
-    }
-
-    /**
-     * index method
-     *
-     * @return void
-     */
-    public function index() {
-        if (!empty($this->request->params['requested'])) {
-            $cats = $this->Category->find('threaded', array(
-                'order' => array('Category.' . $this->request->named['sort'] => $this->request->named['direction']),
-                    //'limit' => $this->request->named['limit'],
-                    )
-            );
-            return $cats;
-        } else {
-            $this->Category->recursive = 0;
-            $this->set('categories', $this->paginate());
-        }
-    }
-
-    /**
-     * view method
-     *
-     * @param string $id
-     * @return void
-     */
-    public function view($id = null) {
-        $this->Category->id = $id;
-        if (!$this->Category->exists()) {
-            throw new NotFoundException(__('Invalid category'));
-        }
-        $this->set('category', $this->Category->read(null, $id));
-    }
-
-    /**
-     * add method
-     *
-     * @return void
-     */
-    public function add() {
-        if ($this->request->is('post')) {
-            $this->Category->create();
-            if ($this->Category->save($this->request->data)) {
-                $this->Session->setFlash(__('The category has been saved'));
-                $this->redirect(array('action' => 'index'));
-            } else {
-                $this->Session->setFlash(__('The category could not be saved. Please, try again.'));
-            }
-        }
-        $parentCategories = $this->Category->ParentCategory->find('list');
-        $posts = $this->Category->Post->find('list');
-        $this->set(compact('parentCategories', 'posts'));
-    }
-
-    /**
-     * edit method
-     *
-     * @param string $id
-     * @return void
-     */
-    public function edit($id = null) {
-        $this->Category->id = $id;
-        if (!$this->Category->exists()) {
-            throw new NotFoundException(__('Invalid category'));
-        }
-        if ($this->request->is('post') || $this->request->is('put')) {
-            if ($this->Category->save($this->request->data)) {
-                $this->Session->setFlash(__('The category has been saved'));
-                $this->redirect(array('action' => 'index'));
-            } else {
-                $this->Session->setFlash(__('The category could not be saved. Please, try again.'));
-            }
-        } else {
-            $this->request->data = $this->Category->read(null, $id);
-        }
-        $parentCategories = $this->Category->ParentCategory->find('list');
-        $posts = $this->Category->Post->find('list');
-        $this->set(compact('parentCategories', 'posts'));
-    }
-
-    /**
-     * delete method
-     *
-     * @param string $id
-     * @return void
-     */
-    public function delete($id = null) {
-        if (!$this->request->is('post')) {
-            throw new MethodNotAllowedException();
-        }
-        $this->Category->id = $id;
-        if (!$this->Category->exists()) {
-            throw new NotFoundException(__('Invalid category'));
-        }
-        if ($this->Category->delete()) {
-            $this->Session->setFlash(__('Category deleted'));
-            $this->redirect(array('action' => 'index'));
-        }
-        $this->Session->setFlash(__('Category was not deleted'));
-        $this->redirect(array('action' => 'index'));
     }
 
     /**
@@ -149,20 +52,6 @@ class CategoriesController extends AppController {
             'contain' => FALSE
         );
         $this->set('categories', $this->paginate());
-    }
-
-    /**
-     * admin_view method
-     *
-     * @param string $id
-     * @return void
-     */
-    public function admin_view($id = null) {
-        $this->Category->id = $id;
-        if (!$this->Category->exists()) {
-            throw new NotFoundException(__('Invalid category'));
-        }
-        $this->set('category', $this->Category->read(null, $id));
     }
 
     /**
