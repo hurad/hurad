02db25e5942ce7d89fa3ff6536009caa0bae93f6
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-03-08 13:36:06 +0330

    Change selector and class.

--- webroot/js/admin/sidebar-menu.js
+++ webroot/js/admin/sidebar-menu.js
@@ -8,22 +8,26 @@ $(document).ready(function()
 //    });
 
     if (Hurad.params.action == "admin_index" || Hurad.params.action == "admin_filter" || Hurad.params.action == "admin_listBycategory" || Hurad.params.action == "admin_listByauthor") {
-        $('a[href$="' + Hurad.params.controller + '"]').parent().parent().parent().parent().addClass("in");//add .in class to div.accordion-body
+        //$('a[href$="' + Hurad.params.controller + '"]').parent().parent().parent().parent().addClass("in");//add .in class to div.accordion-body
         $('a[href$="' + Hurad.params.controller + '"]').parent().addClass("active");
     }
     else if (Hurad.params.action == "admin_add") {
         if (Hurad.params.controller == "linkcats" || Hurad.params.controller == "categories" || Hurad.params.controller == "tags") {
-            $('a[href$="' + Hurad.params.controller + '"]').parent().addClass("current");
+            $('a[href$="' + Hurad.params.controller + '"]').parent().addClass("active");
         }
-        $('a[href$="' + Hurad.params.controller + '/add"]').parent().addClass("current");
+        $('a[href$="' + Hurad.params.controller + '/add"]').parent().addClass("active");
+    }
+    else if (Hurad.params.action == "admin_edit") {
+        if (Hurad.params.controller == "posts" || Hurad.params.controller == "categories" || Hurad.params.controller == "tags") {
+            $('a[href$="' + Hurad.params.controller + '"]').parent().addClass("active");
+        }
     }
     else if (Hurad.params.action == "admin_prefix") {
-        $('a[href$="' + Hurad.params.pass + '"]').parent().addClass("current");
+        $('a[href$="' + Hurad.params.pass + '"]').parent().addClass("active");
     }
     else if (Hurad.params.action == "admin_dashboard") {
-        $('a[href$="admin"]').parent().addClass("current");
+        $('a[href$="admin"]').parent().addClass("active");
     }
 
-    $('li.current').parents('li.sb').css("display", "list-item");
-    $('li.current').parents('ul.menu').find(".top-menu").addClass("active");
+    $('li.active').parent('.nav-list').parent().parent().addClass("in");
 });
