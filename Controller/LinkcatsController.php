99fbd51b1da796e39131bbfbceb99085948f37de
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-03-09 13:05:00 +0330

    Remove not used methods.

--- Controller/LinkcatsController.php
+++ Controller/LinkcatsController.php
@@ -11,107 +11,26 @@ class LinkcatsController extends AppController {
 
     public function beforeFilter() {
         parent::beforeFilter();
-        $this->Auth->allow('*');
-        //$this->isAuthorized();
+        $this->isAuthorized();
     }
 
     public function isAuthorized() {
         switch ($this->Auth->user('role')) {
             case 'admin':
-                $this->Auth->allow('*');
+                $this->Auth->allow();
+                break;
+            case 'editor':
+                $this->Auth->allow('admin_index', 'admin_edit', 'admin_add');
+                break;
+            case 'author':
+                $this->Auth->allow('admin_index');
                 break;
             case 'user':
-                $this->Auth->allow('index', 'profile', 'change_password', 'login', 'logout');
+                $this->Auth->deny();
             default :
-                $this->Auth->allow('login', 'logout', 'view', 'register');
+                return FALSE;
                 break;
         }
-    }
-
-    /**
-     * index method
-     *
-     * @return void
-     */
-    public function index() {
-        $this->Menu->recursive = 0;
-        $this->set('menus', $this->paginate());
-    }
-
-    /**
-     * view method
-     *
-     * @param string $id
-     * @return void
-     */
-    public function view($id = null) {
-        $this->Menu->id = $id;
-        if (!$this->Menu->exists()) {
-            throw new NotFoundException(__('Invalid menu'));
-        }
-        $this->set('menu', $this->Menu->read(null, $id));
-    }
-
-    /**
-     * add method
-     *
-     * @return void
-     */
-    public function add() {
-        if ($this->request->is('post')) {
-            $this->Menu->create();
-            if ($this->Menu->save($this->request->data)) {
-                $this->Session->setFlash(__('The menu has been saved'));
-                $this->redirect(array('action' => 'index'));
-            } else {
-                $this->Session->setFlash(__('The menu could not be saved. Please, try again.'));
-            }
-        }
-    }
-
-    /**
-     * edit method
-     *
-     * @param string $id
-     * @return void
-     */
-    public function edit($id = null) {
-        $this->Menu->id = $id;
-        if (!$this->Menu->exists()) {
-            throw new NotFoundException(__('Invalid menu'));
-        }
-        if ($this->request->is('post') || $this->request->is('put')) {
-            if ($this->Menu->save($this->request->data)) {
-                $this->Session->setFlash(__('The menu has been saved'));
-                $this->redirect(array('action' => 'index'));
-            } else {
-                $this->Session->setFlash(__('The menu could not be saved. Please, try again.'));
-            }
-        } else {
-            $this->request->data = $this->Menu->read(null, $id);
-        }
-    }
-
-    /**
-     * delete method
-     *
-     * @param string $id
-     * @return void
-     */
-    public function delete($id = null) {
-        if (!$this->request->is('post')) {
-            throw new MethodNotAllowedException();
-        }
-        $this->Menu->id = $id;
-        if (!$this->Menu->exists()) {
-            throw new NotFoundException(__('Invalid menu'));
-        }
-        if ($this->Menu->delete()) {
-            $this->Session->setFlash(__('Menu deleted'));
-            $this->redirect(array('action' => 'index'));
-        }
-        $this->Session->setFlash(__('Menu was not deleted'));
-        $this->redirect(array('action' => 'index'));
     }
 
     /**
@@ -123,20 +42,6 @@ class LinkcatsController extends AppController {
         $this->set('title_for_layout', __('Link Categories'));
         $this->Linkcat->recursive = 0;
         $this->set('linkcats', $this->paginate('Linkcat', array('Linkcat.type' => 'link_category')));
-    }
-
-    /**
-     * admin_view method
-     *
-     * @param string $id
-     * @return void
-     */
-    public function admin_view($id = null) {
-        $this->Menu->id = $id;
-        if (!$this->Menu->exists()) {
-            throw new NotFoundException(__('Invalid menu'));
-        }
-        $this->set('menu', $this->Menu->read(null, $id));
     }
 
     /**
