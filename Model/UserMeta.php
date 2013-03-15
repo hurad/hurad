81ec434a6efc1fbc040de389a7fb34fe10fad0db
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-03-15 14:25:33 +0330

    Added UserMeta Model.

---
+++ Model/UserMeta.php
@@ -0,0 +1,17 @@
+<?php
+
+App::uses('AppModel', 'Model');
+
+class UserMeta extends AppModel {
+
+    public $belongsTo = array(
+        'User' => array(
+            'className' => 'User',
+            'foreignKey' => 'user_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        )
+    );
+
+}
