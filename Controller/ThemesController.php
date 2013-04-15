050538860b5ca6c03ea3de132f5a770b54315386
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-04-15 14:10:40 +0430

    Rewrite ThemesController with new Lib (HuradTheme)

--- Controller/ThemesController.php
+++ Controller/ThemesController.php
@@ -1,12 +1,6 @@
 <?php
 
-App::uses('File', 'Utility');
-App::uses('Folder', 'Utility');
-App::uses('HrTheme', 'Lib');
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+App::uses('HuradTheme', 'Lib');
 
 /**
  * Description of ThemesController
@@ -14,13 +8,6 @@ App::uses('HrTheme', 'Lib');
  * @author mohammad
  */
 class ThemesController extends AppController {
-
-    protected $_HrTheme = false;
-
-    public function __construct($request = null, $response = null) {
-        $this->_HrTheme = new HrTheme();
-        parent::__construct($request, $response);
-    }
 
     public function beforeFilter() {
         parent::beforeFilter();
@@ -30,62 +17,39 @@ class ThemesController extends AppController {
     public function admin_index() {
         $this->set('title_for_layout', __('Themes'));
 
-        $themes = $this->_HrTheme->getThemes(TRUE);
-        //$themesData = array();
-        $themesData = $this->_HrTheme->getData();
+        $currentTheme = array();
+        $themes = HuradTheme::getThemeData();
+        $current = Configure::read('template');
 
-        foreach ($themes as $folderName => $themeName) {
-            $themesData[$folderName] = $this->_HrTheme->getData($folderName);
+        if (Configure::check('template') && !empty($current)) {
+            $currentTheme = $themes[Configure::read('template')];
         }
-        $currentTheme = $this->_HrTheme->getData(Configure::read('template'));
-        $this->set(compact('themes', 'themesData', 'currentTheme'));
+
+        $this->set(compact('themes', 'currentTheme'));
     }
 
     public function admin_delete($alias = null) {
         if ($alias == null) {
-            $this->Session->setFlash(__('Invalid Theme.'), 'default', array('class' => 'error'));
+            $this->Session->setFlash(__('Invalid Theme.'), 'error');
             $this->redirect(array('action' => 'index'));
         }
-
-        if ($alias == Configure::read('template')) {
-            $this->Session->setFlash(__('You cannot delete a theme that is currently active.'), 'default', array('class' => 'error'));
+        if (HuradTheme::delete($alias)) {
+            $this->Session->setFlash(__('%s Theme successfuly deleted.', Configure::read('template')), 'success');
             $this->redirect(array('action' => 'index'));
-        }
-
-        $paths = array(
-            APP . 'webroot' . DS . 'theme' . DS . $alias . DS,
-            APP . 'View' . DS . 'Themed' . DS . $alias . DS,
-        );
-
-        $error = 0;
-        $folder = & new Folder;
-        foreach ($paths as $path) {
-            if (is_dir($path)) {
-                if (!$folder->delete($path)) {
-                    $error = 1;
-                }
-            }
-        }
-
-        if ($error == 1) {
-            $this->Session->setFlash(__('An error occurred.'), 'flash_error');
         } else {
-            $this->Session->setFlash(__('Theme deleted successfully.'), 'flash_notice');
+            $this->Session->setFlash(__('Occured error.'), 'error');
+            $this->redirect(array('action' => 'index'));
         }
-
-        $this->redirect(array('action' => 'index'));
     }
 
     public function admin_activate($alias = null) {
-        if ($this->_HrTheme->activate($alias)) {
-            $this->Session->setFlash(__('Theme activated.'), 'default', array('class' => 'success'));
+        if (HuradTheme::activate($alias)) {
+            $this->Session->setFlash(__('Theme activated.'), 'success');
+            $this->redirect(array('action' => 'index'));
         } else {
-            $this->Session->setFlash(__('Theme activation failed.'), 'default', array('class' => 'success'));
+            $this->Session->setFlash(__('Theme activation failed.'), 'error');
+            $this->redirect(array('action' => 'index'));
         }
-
-        $this->redirect(array('action' => 'index'));
     }
 
 }
-
-?>
