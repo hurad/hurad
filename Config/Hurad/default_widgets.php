517c83ade89b255a280b4caf110e516713ea02fd
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-04-17 23:02:21 +0430

    Add default filters and widgets.

---
+++ Config/Hurad/default_widgets.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Author widget
+ * Show author's list
+ */
+HuradWidget::registerWidget(array(
+    'title' => __('Authors'),
+    'element' => 'authors',
+        )
+);
+HuradWidget::registerWidget(array(
+    'title' => __('Categories'),
+    'element' => 'categories',
+        )
+);
+HuradWidget::registerWidget(array(
+    'title' => __('Recent Posts'),
+    'element' => 'recent_posts',
+        )
+);
+HuradWidget::registerWidget(array(
+    'title' => __('Pages'),
+    'element' => 'pages',
+        )
+);
