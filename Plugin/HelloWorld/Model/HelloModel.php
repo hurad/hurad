8499066b91f9f2b42109bd374aa3b89407020cef
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-05-02 19:44:14 +0430

    Change var to public.

--- Plugin/HelloWorld/Model/HelloModel.php
+++ Plugin/HelloWorld/Model/HelloModel.php
@@ -1,17 +1,12 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of HelloModel
  *
  * @author mohammad
  */
 class HelloModel extends HelloWorldAppModel {
-    var $name = 'Hello';
-}
 
-?>
+    public $name = 'Hello';
+
+}
