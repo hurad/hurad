a5ce3b8434c3a9201bbbc4dbaa8eba195933e99b
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-06-07 15:25:35 +0430

    Added testGetUsers method.

--- Test/Case/Model/UserTest.php
+++ Test/Case/Model/UserTest.php
@@ -23,6 +23,51 @@ class UserTest extends CakeTestCase {
         $this->assertEquals($created, '2007-03-17 01:16:23', 'Created Date');
     }
 
+    public function testGetUsers() {
+        $args = array(
+            'orderby' => 'created',
+            'order' => 'asc',
+            'number' => 2
+        );
+
+        $result = $this->User->getUsers($args);
+
+        $expected = array(
+            0 => array(
+                'User' => array(
+                    'id' => '1',
+                    'username' => 'admin',
+                    'password' => '5f4dcc3b5aa765d61d8327deb882cf99',
+                    'email' => 'admin@example.org',
+                    'url' => '',
+                    'role' => 'admin',
+                    'activation_key' => '',
+                    'reset_key' => '',
+                    'status' => '0',
+                    'created' => '2007-03-17 01:16:23',
+                    'modified' => '2007-03-17 01:18:31',
+                )
+            ),
+            1 => array(
+                'User' => array(
+                    'id' => '2',
+                    'username' => 'editor',
+                    'password' => '5f4dcc3b5aa765d61d8327deb882cf99',
+                    'email' => 'editor@example.org',
+                    'url' => '',
+                    'role' => 'editor',
+                    'activation_key' => '',
+                    'reset_key' => '',
+                    'status' => '0',
+                    'created' => '2007-03-17 01:18:23',
+                    'modified' => '2007-03-17 01:20:31',
+                )
+            )
+        );
+
+        $this->assertEquals($expected, $result);
+    }
+
     public function testGetUserData() {
         $result = $this->User->getUserData(1);
 
