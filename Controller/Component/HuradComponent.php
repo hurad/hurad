61d33333a0f0efccfa732c2aead131a5b61f0eb1
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-09-04 00:10:15 +0430

    Add HuradComponent

---
+++ Controller/Component/HuradComponent.php
@@ -0,0 +1,41 @@
+<?php
+
+App::uses('Component', 'Controller');
+App::uses('CakeEmail', 'Network/Email');
+
+/**
+ * Description of Hurad
+ *
+ * @author mohammad
+ */
+class HuradComponent extends Component
+{
+
+    public function sendEmail(
+        $to,
+        $subject,
+        $template = 'default',
+        $content = null,
+        $viewVars = array(),
+        $options = array()
+    ) {
+        $defaults = array(
+            'config' => 'smtp',
+            'emailFormat' => 'html'
+        );
+
+        $options = array_merge($defaults, $options);
+
+        $email = new CakeEmail();
+        $email->config($options['config']);
+        $email->from('info@hurad.org', Configure::read('General.site_name'));
+        $email->to($to);
+        $email->setHeaders(array('X-Mailer' => "Hurad Mail"));
+        $email->emailFormat($options['emailFormat']);
+        $email->template($template);
+        $email->viewVars($viewVars);
+        $email->subject($subject);
+        $email->send($content);
+    }
+
+}
