9285605c2270b8e9c9224f8cfa41b243899c8b90
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-09-26 23:54:58 +0330

    Added composer autoload.

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -207,6 +207,16 @@ define('HURAD_CONFIG', CONFIG . 'Hurad' . DS);
 
 Configure::write('Installed', file_exists(TMP . 'installed'));
 
+if (file_exists(APP . '/Vendor/autoload.php')) {
+    // Load composer autoload.
+    require APP . '/Vendor/autoload.php';
+
+// Remove and re-prepend CakePHP's autoloader as composer thinks it is the most important.
+// See https://github.com/composer/composer/commit/c80cb76b9b5082ecc3e5b53b1050f76bb27b127b
+    spl_autoload_unregister(array('App', 'load'));
+    spl_autoload_register(array('App', 'load'), true, true);
+}
+
 if (Configure::read('Installed')) {
     config('Hurad/bootstrap');
 }
