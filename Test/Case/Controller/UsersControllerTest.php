51e087f6001693f08e2f93c7c4a7d8c77fe66b61
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-10-10 22:47:08 +0330

    Add fixtures Edit phpDoc

--- Test/Case/Controller/UsersControllerTest.php
+++ Test/Case/Controller/UsersControllerTest.php
@@ -3,31 +3,18 @@
 App::uses('UsersController', 'Controller');
 
 /**
- * Description of UsersControllerTest
- *
- * @author mohammad
+ * Class UsersControllerTest
  */
 class UsersControllerTest extends ControllerTestCase
 {
 
-    public $fixtures = array('app.user', 'app.userMeta');
+    public $fixtures = array('app.user', 'app.userMeta', 'app.comment', 'app.category', 'app.tag', 'app.post');
     public $User;
-
-    public function setUp()
-    {
-        parent::setUp();
-    }
-
-    public function startTest($method)
-    {
-        parent::startTest($method);
-    }
 
     public function testAdminIndex()
     {
         $this->testAction('/admin/users/index');
         $this->assertInternalType('array', $this->vars['users']);
-        debug($this->vars['users']);
     }
 
 }
