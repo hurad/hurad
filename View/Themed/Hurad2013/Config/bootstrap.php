d2c43a43becf8d509b77750c4265450715ffaef5
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2013-10-25 00:06:31 +0330

    Change default widget markup

--- View/Themed/Hurad2013/Config/bootstrap.php
+++ View/Themed/Hurad2013/Config/bootstrap.php
@@ -4,6 +4,9 @@ HuradWidget::registerSidebar(
     array(
         'name' => __('Right Sidebar'),
         'id' => 'right-sidebar',
-        'class' => 'well'
+        'before_widget' => '<div class="panel panel-default">',
+        'after_widget' => '</div></div>',
+        'before_title' => '<div class="panel-heading">',
+        'after_title' => '</div><div class="panel-body">',
     )
 );
